8bae576bd47356cd121b643175a56425099b0596
brenodiogo <breno.diogo@hotmail.com>
2019-09-05 17:13:45 -0300

    adiciona metodos para conectar com bluetooth

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { ToastController } from "@ionic/angular";
 import { BluetoothSerial } from "@ionic-native/bluetooth-serial/ngx";
+import { LoadingController } from "@ionic/angular";
 
 @Component({
   selector: "app-home",
@@ -8,9 +9,12 @@ import { BluetoothSerial } from "@ionic-native/bluetooth-serial/ngx";
   styleUrls: ["home.page.scss"]
 })
 export class HomePage {
+  public listOfBluetoothDevices: any;
+
   constructor(
     public toastController: ToastController,
-    public bluetoothSerial: BluetoothSerial
+    public bluetoothSerial: BluetoothSerial,
+    public loadingController: LoadingController
   ) {}
 
   ativarBluetooth() {
@@ -25,7 +29,37 @@ export class HomePage {
     );
   }
 
-  listarBluetooth() {}
+  async listarBluetooth() {
+    const loader = await this.presentLoading();
+    await loader.present();
+    try {
+      const listOfBluetoothDevices = await this.bluetoothSerial.list();
+      this.listOfBluetoothDevices = listOfBluetoothDevices;
+      this.presentToast("Obtive lista de Bluetooth com sucesso!");
+    } catch (error) {
+      this.presentToast(error);
+      return error;
+    } finally {
+      await loader.dismiss();
+    }
+  }
+
+  async conectarComDispositivo(address: string) {
+    const loader = await this.presentLoading();
+    await loader.present();
+    this.presentToast("Conectando com " + address);
+    this.bluetoothSerial
+      .connect(address)
+      .subscribe(
+        async success => {
+          await loader.dismiss();
+        },
+        async error => {
+          await loader.dismiss();
+          console.log("erro" + error)
+        }
+      );
+  }
 
   async presentToast(message) {
     const toast = await this.toastController.create({
@@ -33,5 +67,14 @@ export class HomePage {
       duration: 2000
     });
     toast.present();
+  }
+
+  async presentLoading() {
+    const loading = await this.loadingController.create({
+      message: "Please wait...",
+      translucent: true,
+      cssClass: "custom-class custom-loading"
+    });
+    return loading;
   }
 }
