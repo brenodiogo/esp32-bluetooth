4668d053ab73f0cad0d1b7d81d6b40827eb9ea74
brenodiogo <breno.diogo@hotmail.com>
2019-09-05 15:50:46 -0300

    adiciona plugin bluetooth

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ToastController } from "@ionic/angular";
+import { BluetoothSerial } from "@ionic-native/bluetooth-serial/ngx";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
 export class HomePage {
+  constructor(
+    public toastController: ToastController,
+    public bluetoothSerial: BluetoothSerial
+  ) {}
 
-  constructor() {}
+  ativarBluetooth() {
+    this.presentToast("Ativar Bluetooth Clicado");
+    this.bluetoothSerial.enable().then(
+      () => {
+        this.presentToast("Ativar Bluetooth Sucesso");
+      },
+      () => {
+        this.presentToast("Ativar Bluetooth Erro");
+      }
+    );
+  }
 
+  listarBluetooth() {}
+
+  async presentToast(message) {
+    const toast = await this.toastController.create({
+      message,
+      duration: 2000
+    });
+    toast.present();
+  }
 }
