274ef681dcee70df5f570afe5dc1df76c9bf8419
brenodiogo <breno.diogo@hotmail.com>
2019-09-06 11:47:20 -0300

    adiciona funcao para escrever dados por bluetooth

--- src/app/pages/dispositivo/dispositivo.page.ts
+++ src/app/pages/dispositivo/dispositivo.page.ts
@@ -1,18 +1,103 @@
-import { Component, OnInit } from '@angular/core';
-import { BluetoothService } from 'src/app/services/bluetooth-service.service';
+import { Component, OnInit } from "@angular/core";
+import { BluetoothService } from "src/app/services/bluetooth-service.service";
+import { BluetoothSerial } from "@ionic-native/bluetooth-serial/ngx";
+import { ToastController, LoadingController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-dispositivo',
-  templateUrl: './dispositivo.page.html',
-  styleUrls: ['./dispositivo.page.scss'],
+  selector: "app-dispositivo",
+  templateUrl: "./dispositivo.page.html",
+  styleUrls: ["./dispositivo.page.scss"]
 })
 export class DispositivoPage implements OnInit {
   connectedDevice: string;
+  ligaDesliga: boolean = false;
 
-  constructor(private bluetoothService: BluetoothService) { }
+  constructor(
+    private bluetoothService: BluetoothService,
+    public bluetoothSerial: BluetoothSerial,
+    public toastController: ToastController,
+    public loadingController: LoadingController
+  ) {}
 
   ngOnInit() {
     this.connectedDevice = this.bluetoothService.getConnectedDevice();
   }
 
+  async conectarDispositivo() {
+    const loader = await this.createLoader();
+    loader.present();
+    this.bluetoothSerial.connect(this.connectedDevice).subscribe(
+      success => {
+        loader.dismiss();
+        console.log("sucesso em conexão");
+        console.log(success);
+        this.presentToast(success);
+      },
+      error => {
+        loader.dismiss();
+        console.error("erro" + error);
+        console.error(error);
+        console.log(error);
+        this.presentToast(error);
+      }
+    );
+  }
+
+  async escreverDados() {
+    this.ligaDesliga = !this.ligaDesliga;
+    const loader = await this.createLoader();
+    let message = this.ligaDesliga ? "ligar" : "desligar";
+    loader.present();
+    this.bluetoothSerial.write(message).then(
+      success => {
+        loader.dismiss();
+        this.presentToast("Comando enviado: " + message);
+        console.log("Comando enviado: " + message);
+        console.log(message);
+        console.log(success);
+      },
+      error => {
+        loader.dismiss();
+        this.presentToast("Erro ao enviar mensagem: " + message + " . Erro: " + error);
+        console.error("Erro ao enviar mensagem: " + message + " . Erro: " + error);
+        console.error(error);
+      }
+    );
+  }
+
+  async testarConexao() {
+    const loader = await this.createLoader();
+    loader.present();
+    this.bluetoothSerial.isConnected().then(
+      success => {
+        loader.dismiss();
+        this.presentToast("Bluetooth is connected");
+        console.log("Bluetooth is connected");
+        console.log(success);
+      },
+      error => {
+        loader.dismiss();
+        this.presentToast("Bluetooth is not connected");
+        console.error("Bluetooth is not connected");
+        console.error(error);
+      }
+    );
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message,
+      duration: 4000
+    });
+    toast.present();
+  }
+
+  async createLoader() {
+    const loader = await this.loadingController.create({
+      message: "Please wait...",
+      translucent: true,
+      cssClass: "custom-class custom-loading"
+    });
+    return loader;
+  }
 }
